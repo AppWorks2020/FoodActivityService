70b56c04945b8790f9b259a34935506a18472392
Arun <AppWorks2020@gmail.com>
2020-08-30 23:26:57 +0530

    initial commit

---
+++ src/main/java/org/appworks2020/foodactivityservice/FoodActivityServiceApplication.java
@@ -0,0 +1,13 @@
+package org.appworks2020.foodactivityservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodActivityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodActivityServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/appworks2020/foodactivityservice/FoodActivityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.appworks2020.foodactivityservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodActivityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
